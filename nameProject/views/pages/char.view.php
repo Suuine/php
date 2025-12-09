d3471c8bb495c23d8c738bb920caee0a826897e0
Богач Надія <114477404+Suuine@users.noreply.github.com>
2025-12-09 14:37:50 +0200

    Update char.view.php

--- nameProject/views/pages/char.view.php
+++ nameProject/views/pages/char.view.php
@@ -7,12 +7,7 @@
         </li>
     <?php endforeach; ?>
 </ul>
-<?php /*
-<?php
-$pagination = ['count' => 150, 'perPage' => 15];
-?>
-<?php var_dump(($pagination['count'] / $pagination['perPage'])); ?>
-*/ ?>
+
 <?php for($x = 1; $x < ($pagination['count'] / $pagination['perPage'] + 1); $x++): ?>
     <a class="button" href="char.php?<?php echo http_build_query(['char' => $char, 'page' => $x]); ?>">
         <?php if ($x === $pagination['page']): ?>
@@ -22,8 +17,5 @@ $pagination = ['count' => 150, 'perPage' => 15];
         <?php endif; ?>
     </a>
 <?php endfor; ?>
-<?php /*
 
-<?php var_dump($pagination); ?>
-*/ ?>
 <br /><br /><br /><br /><br />
